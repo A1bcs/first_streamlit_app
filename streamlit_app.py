fedc0950b9c0fc5c944e7107992e2bb2b81f91e3
saranya rajendran <109729360+A1bcs@users.noreply.github.com>
2022-07-21 17:05:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,25 @@
 import streamlit
-streamlit.title('my parents new healthy dinner')
-streamlit.header('breakfast menu')
-streamlit.title('blueberry')
-streamlit.title('spinach')
-streamlit.title('boiled egg')
-streamlit.header('build your own fruit smoothie')
- 
- import pandas
- my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
- streamlit.dataframe(my_fruit_list)
+
+streamlit.title('my parents new healthy diner')
+
+streamlit.header('Breakfast favorites')
+streamlit.text('🥣 Omega 3 & blueberry oatmeal')
+streamlit.text('🥗 Kale spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+# Display the table on the page.
+streamlit.dataframe(fruits_to_show)
+
+
+https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt

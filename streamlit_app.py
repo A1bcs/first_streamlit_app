d819a843be4365dd611c22bf08846af05a3743f4
saranya rajendran <109729360+A1bcs@users.noreply.github.com>
2022-07-21 16:57:33 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,6 @@
-
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-
-https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt
+import streamlit
+streamlit.title('my parents new healthy dinner')
+streamlit.header('breakfast menu')
+streamlit.title('blueberry')
+streamlit.title('spinach')
+streamlit.title('boiled egg')

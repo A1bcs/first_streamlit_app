e296978eec0d19c3dd386205d014774dbb07d72c
saranya rajendran <109729360+A1bcs@users.noreply.github.com>
2022-07-21 15:35:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,9 @@ streamlit.text('spinach')
 streamlit.text('boiled egg')
 streamlit.text('build your own fruit smoothie')
 streamlit.header('build your own fruit smoothie')
+
+
+
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)

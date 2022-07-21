4c8f13d7a3f4d12653990da984a0f44ceb14b36a
saranya rajendran <109729360+A1bcs@users.noreply.github.com>
2022-07-21 15:25:00 +0530

    update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('my parents new healthy dinner')
+streamlit.header('breakfast menu')
+streamlit.text('blueberry')
+streamlit.text('spinach')
+streamlit.text('boiled egg')

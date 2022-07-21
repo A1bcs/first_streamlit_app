be4a049c2a695b707e08d15c51cf58f5667a1dfa
saranya rajendran <109729360+A1bcs@users.noreply.github.com>
2022-07-21 16:02:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,2 @@
 import streamlit
 
-streamlit.title('my parents new healthy dinner')
-streamlit.header('breakfast menu')
-streamlit.text('blueberry')
-streamlit.text('spinach')
-streamlit.text('boiled egg')
-streamlit.text('build your own fruit smoothie')
-streamlit.header('build your own fruit smoothie')
-
-import pandas
-my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','strawberries'])
-# Display the table on the page
-
-streamlit.dataframe(my_fruit_list)
-
